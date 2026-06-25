18fcf1ee3f71d5ab769e849009e68e120e23fa35
DaiChang <diyouyou345@outlook.com>
2026-06-25 10:15:52 +0800

    feat: 改进原版组件类型化值编辑

--- src/main/java/io/github/seraphina/infinity_item_editor_re/client/screen/ItemEditorScreen.java
+++ src/main/java/io/github/seraphina/infinity_item_editor_re/client/screen/ItemEditorScreen.java
@@ -48,6 +48,8 @@ public class ItemEditorScreen extends ItemEditorScreenRendering {
         this.nameBox = null;
         this.rawNbtBox = null;
         this.componentFilterBox = null;
+        this.componentValueSearchBox = null;
+        this.componentNumberBox = null;
         this.componentNbtBox = null;
         this.enchantFilterBox = null;
         this.enchantLevelBox = null;
@@ -89,6 +91,9 @@ public class ItemEditorScreen extends ItemEditorScreenRendering {
         this.redSlider = null;
         this.greenSlider = null;
         this.blueSlider = null;
+        this.componentRedSlider = null;
+        this.componentGreenSlider = null;
+        this.componentBlueSlider = null;
         this.copyLoreButton = null;
 
         switch (this.activePanel) {
@@ -236,6 +241,9 @@ public class ItemEditorScreen extends ItemEditorScreenRendering {
         }
         if (this.activePanel == Panel.COMPONENTS && this.componentFilterBox != null && this.componentFilterBox.isFocused()) {
             return this.componentFilterBox.charTyped(new CharacterEvent(Character.toLowerCase(codePoint), modifiers));
+        }
+        if (this.activePanel == Panel.COMPONENTS && this.componentValueSearchBox != null && this.componentValueSearchBox.isFocused()) {
+            return this.componentValueSearchBox.charTyped(new CharacterEvent(Character.toLowerCase(codePoint), modifiers));
         }
         return super.charTyped(codePoint, modifiers);
     }
